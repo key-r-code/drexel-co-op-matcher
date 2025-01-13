7a7d9cfe865af42f848a02ce8319edbc955c23a7
key-r-code <kramjattun@gmail.com>
2025-01-12 22:34:05 -0500

    updated main.py

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ from dragonScraper import dragonScraper
 if __name__ == "__main__":
 
     scraper = dragonScraper()
-    scraper.grab_interview(USER, PASSWORD)
 
+    # add login details
+    scraper.login_to_banner(username='', password='')
+
+    # choose operating system (Mac/Windows)
+    operating_sys = 'Mac'
+
+    # select majors, see majors.json for full code list
+    majors = ["CO-COMP", "CO-DS", "EN-COM", "EN-ELEC"]
+    
+    scraper.grab_job_postings(operating_sys=operating_sys, majors=majors)
     scraper.scrape_all_postings()
